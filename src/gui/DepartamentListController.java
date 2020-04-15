a4107f4781a07d7f8564c3583a43816202f7b6bc
lucasrodrigues19 <pereirarodriguesl194@gmail.com>
2020-04-15 03:38:08 -0300

    Atualização de dados

--- src/gui/DepartamentListController.java
+++ src/gui/DepartamentListController.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
-import db.DB;
 import gui.helper.WorkShopHelper;
 import gui.listeners.DadoAlteradoListener;
 import gui.utils.Alerts;
 import gui.utils.WorkUtils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,20 +17,21 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.stage.Stage;
-import modelo.dao.DaoFactory;
 import modelo.entites.Departamento;
 import modelo.service.DepartamentoService;
 
-public class DepartamentListController implements Initializable,DadoAlteradoListener {
+public class DepartamentListController implements Initializable, DadoAlteradoListener {
 
 	private WorkShopHelper helper = new WorkShopHelper();
-	
+
 	private DepartamentoService service;
 
 	@FXML
@@ -41,6 +42,9 @@ public class DepartamentListController implements Initializable,DadoAlteradoList
 
 	@FXML
 	private TableColumn<Departamento, String> tableColumnName;
+
+	@FXML
+	private TableColumn<Departamento, Departamento> tableColumnEdit;
 
 	@FXML
 	private Button btNew;
@@ -60,33 +64,36 @@ public class DepartamentListController implements Initializable,DadoAlteradoList
 	public void onBtNewAction(ActionEvent event) {
 		Stage stage = WorkUtils.palcoAtual(event);
 		System.out.println("Botao neew...");
-		helper.criarDialogForm(stage, "/gui/DepartamentForm.fxml","Entre com os dados do departamento", (DepartamentFormController controller)->{
-			controller.setDpt(new Departamento());
-			controller.setService(getService());
-			controller.inscreverDadoAlteradoListener(this);//me escrevendo(this)para receber o evento
-			
-		});
+		helper.criarDialogForm(stage, "/gui/DepartamentForm.fxml", "Entre com os dados do departamento",
+				(DepartamentFormController controller) -> {
+					controller.setDpt(new Departamento());
+					controller.setService(getService());
+					inscreverMeuObjeto(controller);// me escrevendo(this)para receber o evento
+
+				});
 	}
 
 	@FXML
-	public void tableColumnIdAction() {
-		tableViewDepartamento.setOnMouseClicked((MouseEvent event) ->{
-			if(event.getButton().equals(MouseButton.PRIMARY)) {
-				if(event.getClickCount() == 2) {
+	public void onTableDuploClick() {
+		tableViewDepartamento.setOnMouseClicked((MouseEvent event) -> {
+			if (event.getButton().equals(MouseButton.PRIMARY)) {
+				if (event.getClickCount() == 2) {
 					String nome = tableViewDepartamento.getSelectionModel().getSelectedItem().getNome();
 					int id = tableViewDepartamento.getSelectionModel().getSelectedItem().getId();
 					Stage stage = WorkUtils.palcoAtual(event);
-					helper.criarDialogForm(stage, "/gui/DepartamentForm.fxml", "Atualizar dados", (DepartamentFormController controller)->{
-						controller.setDpt(new Departamento(id, nome));
-						controller.setService(getService());
-						controller.atualizarDadosForm();
-						controller.inscreverDadoAlteradoListener(this);//me escrevendo(this)para receber o evento(onDadosAlterados)
-					});
-					
+					helper.criarDialogForm(stage, "/gui/DepartamentForm.fxml", "Atualizar dados",
+							(DepartamentFormController controller) -> {
+								controller.setDpt(new Departamento(id, nome));
+								controller.setService(getService());
+								controller.atualizarDadosForm();
+								inscreverMeuObjeto(controller);// me escrevendo(this)para receber o
+																// evento(onDadosAlterados)
+							});
+
 				}
 			}
 		});
-		
+
 	}
 
 	@Override
@@ -126,13 +133,47 @@ public class DepartamentListController implements Initializable,DadoAlteradoList
 
 		obsListDpt = FXCollections.observableArrayList(result);
 		tableViewDepartamento.setItems(obsListDpt);
+		iniciarBotaoEditar();
+	}
+
+	/**
+	 * Metodo para criar um botao de edicao para cada linha da minha tabela
+	 */
+	public void iniciarBotaoEditar() {
+		tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEdit.setCellFactory(param -> new TableCell<Departamento, Departamento>() {
+			private final Button button = new Button("Editar");
+
+			@Override
+			protected void updateItem(Departamento obj, boolean empty) {
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(event -> helper.criarDialogForm(WorkUtils.palcoAtual(event),
+						"/gui/DepartamentForm.fxml", "Atualizar dados", (DepartamentFormController controller) -> {
+							controller.setDpt(obj);
+							inscreverMeuObjeto(controller);
+							controller.setService(getService());
+							controller.atualizarDadosForm();
+						})
+
+				);
+				
+			}
+		});
 	}
 
 	@Override
 	public void onDadosAlterados() {
 		atualizarTableView();
-		
+
 	}
 
-	
+	public void inscreverMeuObjeto(DepartamentFormController controller) {
+		controller.inscreverDadoAlteradoListener(this);
+	}
+
 }
